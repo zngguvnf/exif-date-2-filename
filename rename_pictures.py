e2dd6fb90d90c20ed928fb7c98f33e5c49c905b4
zngguvnf <git@zngguvnf.org>
2017-07-30 12:20:08 +0200

    adds basic functionallity

--- rename_pictures.py
+++ rename_pictures.py
@@ -1,13 +1,38 @@
 import os
 import shutil
+import exifread
 
 
-def rename_pictures(source='testfiles/source',
-                    destination='testfiles/destination'):
+def convert_timestamp(ExifTime):
+    # converts Exif Timestamp '2015:12:27 09:43:44'
+    # To ISO like Timestamp   '2015-12-27T09-43-44'
+    #attrs = dir(ExifTime)
+    values = getattr(ExifTime, 'values')
+    # values = '2015:12:27 09:43:44'
+    timestamp = values.replace(' ', 'T')
+    timestamp = timestamp.replace(':', '-')
+    return timestamp
+
+
+def rename_pictures(source='htc-desire',
+                    destination='htc-desire_out'):
 
     for filename in os.listdir(source):
-        shutil.copy(source + '/' + filename, destination)
+        if filename != '.DS_Store':
+            extension = os.path.splitext(filename)[1]
 
+            f = open(source+'/'+filename, 'rb')
+            tags = exifread.process_file(f)
+
+            if 'EXIF DateTimeOriginal' in tags.keys():
+                ExifTime = tags['EXIF DateTimeOriginal']
+                timestamp = convert_timestamp(ExifTime)
+
+                shutil.copy(source + '/'
+                            + filename, destination
+                            + '/' + timestamp + extension)
+            else:
+                print('no EXIF Data found for %s' % filename)
 
 if __name__ == "__main__":
     rename_pictures()
